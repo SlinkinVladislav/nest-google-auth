673b9306f895ef0b7073d911d1334e679ba7ec00
SlinkinVladislav <slin.vladislav@gmail.com>
2022-12-06 13:38:10 +0400

    initial commit

---
+++ src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  googleLogin(req) {
+    if (!req.user) {
+      return 'No user from google';
+    }
+    return req.user.email;
+  }
+}
